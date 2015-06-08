d4441188cf2d875cfe49438d72d84db9241ac435
Home_YoonDesk <Administrator@mail.com>
2015-06-08 12:03:05 +0900

    리눅스에 clone

--- Vertx3Project/src/com/vertxstudy/main/BasicVerticle.java
+++ Vertx3Project/src/com/vertxstudy/main/BasicVerticle.java
@@ -3,7 +3,7 @@ package com.vertxstudy.main;
 import io.vertx.core.AbstractVerticle;
 
 public class BasicVerticle extends AbstractVerticle {
-
+//굿굿
 	@Override
 	public void start() throws Exception {
 		// TODO Auto-generated method stub
